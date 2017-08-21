8d9d08b928e33b3fc70b4a7afd4c2365aa3d0571
QuantumSpark <jamespark1234@hotmail.com>
2017-08-20 18:25:54 -0700

    Initial Commit

---
+++ c4Test/WorkSpace.swift
@@ -0,0 +1,19 @@
+//
+//  WorkSpace.swift
+//  c4Test
+//
+//  Created by James Park on 2017-08-20.
+//  Copyright © 2017 James Park. All rights reserved.
+//
+
+import UIKit
+
+class WorkSpace: CanvasController {
+
+    override func setup() {
+        //Work your magic here.
+    }
+
+
+}
+
